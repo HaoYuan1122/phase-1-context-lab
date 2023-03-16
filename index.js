d186580ca86d97eada790063c345fd6c3f61d6b6
Hao Yuan <haomichaelyuan@gmail.com>
2023-03-16 17:15:39 +1100

    commit tree e8e89f98d65b4e425a2a99b9a3d24d4200706b15

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    let obj={
+        firstName:array[0],
+        familyName:array[1],
+        title:array[2],
+        payPerHour:array[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+    return obj
+}
+function createEmployeeRecords(arrays){
+    return arrays.map(createEmployeeRecord)
+}
+function createTimeInEvent(employeeRecord, dateStamp){
+    let obj={}
+    obj.type='TimeIn'
+    obj.hour=parseInt(dateStamp.split(' ')[1])
+    obj.date=dateStamp.split(' ')[0]
+    employeeRecord.timeInEvents.push(obj)
+    return employeeRecord
+}
+function createTimeOutEvent(employeeRecord, dateStamp){
+    let obj={}
+    obj.type='TimeOut'
+    obj.hour=parseInt(dateStamp.split(' ')[1])
+    obj.date=dateStamp.split(' ')[0]
+    employeeRecord.timeOutEvents.push(obj)
+    return employeeRecord
+}
+// function hoursWorkedOnDate(employeeRecord, date){
+//     const timeInOjb=employeeRecord.timeInEvents.find(e=>e.date===date)
+//     const timeOutOjb=employeeRecord.timeOutEvents.find(e=>e.date===date)
+//     return (timeOutOjb.hour-timeInOjb.hour)/100
+// }
+// function wagesEarnedOnDate(employeeRecord, date){
+//     return hoursWorkedOnDate(employeeRecord, date)*employeeRecord.payPerHour
+// }
+// function allWagesFor(employeeRecord){
+//     const dates=employeeRecord.timeInEvents.map(e=>e.date)
+//     const wagesOnEachDate=dates.map(date=>wagesEarnedOnDate(employeeRecord, date))
+//     return wagesOnEachDate.reduce((accumulator,element)=>{
+//         accumulator+=element
+//         return accumulator
+//     },0)
+// }
+
+// function calculatePayroll(arrayOfEmployeeRecords){
+//     const wagesForEachEmployee=arrayOfEmployeeRecords.map(allWagesFor)
+//     return wagesForEachEmployee.reduce((accumulator,element)=>{
+//         accumulator+=element
+//         return accumulator
+//     },0)
+// }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
